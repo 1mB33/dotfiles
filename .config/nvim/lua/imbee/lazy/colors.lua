ddcc50fdd7af7f70cda7da77409512939697fb0b
1mb33 <matt.ib.g.12@gmail.com>
2026-06-26 20:22:10 +0200

    tmux restore, and new colors

--- .config/nvim/lua/imbee/lazy/colors.lua
+++ .config/nvim/lua/imbee/lazy/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-    color = color or "moonfly" -- "tokyodark" -- "rose-pine"
+    color = color or "teide-darker" -- "tokyodark" -- "rose-pine"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -72,4 +72,5 @@ return {
     },
     { 'kepano/flexoki-neovim', name = 'flexoki' },
     { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+    { "serhez/teide.nvim", lazy = false, priority = 1000, opts = {}, }
 }
